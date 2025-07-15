23eefb503831c3e69b30c16fa33d7df0c8178e8e
Beck <Beck@GARDEN>
2025-07-14 20:23:01 -0400

    Add project files.

---
+++ Maths.cs
@@ -0,0 +1,88 @@
+﻿/*
+Overloading: Write a simple Maths class ( don’t use the keyword Math, it will be a conflict with standard class from system). 
+    Write overloaded methods with logic and give choice to user to call different methods :
+a. Add(int num1, int num2)
+b. Add(decimal num1, decimal num2, decimal num3)
+c. Multiply(float num1, float num2)
+d. Multiply( float num1, float num2, float num3)
+Declare these methods as public and static.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2._2._2
+{
+    public class Maths
+    {
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static decimal Add(decimal num1 = 0, decimal num2 = 0, decimal num3 = 0)
+        {
+            return num1 + num2 + num3;
+        }
+
+        public static float Multiply(float num1, float num2)
+        {
+            return num1 * num2;
+        }
+
+        public static float Multiply(float num1 = 1, float num2 = 1, float num3 = 1)
+        {
+            return num1 * num2 * num3;
+        }
+
+        public static decimal GetUserNumber(string dataType)
+        {
+            int userInteger;
+            decimal userNumber;
+            bool validEntry = true;
+
+            if (dataType == "int")
+                Console.Write("Please enter an integer: ");
+            else Console.Write("Please enter a number: ");
+
+            do
+            {
+                string userInput = Console.ReadLine();
+                switch (dataType)
+                {
+                    case "int":
+                        if (int.TryParse(userInput, out userInteger))
+                        {
+                            validEntry = true;
+                            return userInteger;
+                        }
+                        else
+                        {
+                            Console.Write("Invalid entry. Please enter an integer: ");
+                            validEntry = false;
+                        }
+                        break;
+
+                    case "decimal":
+                    case "float":
+                        if (Decimal.TryParse(userInput, out userNumber))
+                        {
+                            validEntry = true;
+                            return userNumber;
+                        }
+                        else
+                        {
+                            Console.Write("Invalid entry. Please enter a number: ");
+                            validEntry = false;
+                        }
+                        break;
+                }
+            } while (!validEntry);
+            return 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿/*
+Overloading: Write a simple Maths class ( don’t use the keyword Math, it will be a conflict with standard class from system). 
+    Write overloaded methods with logic and give choice to user to call different methods :
+a. Add(int num1, int num2)
+b. Add(decimal num1, decimal num2, decimal num3)
+c. Multiply(float num1, float num2)
+d. Multiply( float num1, float num2, float num3)
+Declare these methods as public and static.
+ */
+
+namespace Assignment_2._2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit = false;
+            decimal num1;
+            decimal num2;
+            decimal num3;
+
+            while (!exit)
+            {
+                bool validEntry = true;
+
+                Console.WriteLine("a. Add two numbers (integer) together.");
+                Console.WriteLine("b. Add three numbers (decimal) together.");
+                Console.WriteLine("c. Multiply two numbers (float) together.");
+                Console.WriteLine("d. Multiply three numbers (float) together.");
+                Console.Write("Please select one of the four options or enter 'e' to exit: ");
+
+                do
+                {
+                    switch (Console.ReadLine().ToLower())
+                    {
+                        case "a":
+                            num1 = Maths.GetUserNumber("int");
+                            num2 = Maths.GetUserNumber("int");
+                            Console.WriteLine("The answer is: "+Maths.Add(Convert.ToInt32(num1), Convert.ToInt32(num2)));
+                            validEntry = true;
+                            break;
+                        case "b":
+                            num1 = Maths.GetUserNumber("decimal");
+                            num2 = Maths.GetUserNumber("decimal");
+                            num3 = Maths.GetUserNumber("decimal");
+                            Console.WriteLine("The answer is: " + Maths.Add(Convert.ToDecimal(num1), Convert.ToDecimal(num2), Convert.ToDecimal(num3)));
+                            validEntry = true;
+                            break;
+                        case "c":
+                            num1 = Maths.GetUserNumber("float");
+                            num2 = Maths.GetUserNumber("float");
+                            Console.WriteLine("The answer is: " + Maths.Multiply(Convert.ToSingle(num1), Convert.ToSingle(num2)));
+                            validEntry = true;
+                            break;
+                        case "d":
+                            num1 = Maths.GetUserNumber("float");
+                            num2 = Maths.GetUserNumber("float");
+                            num3 = Maths.GetUserNumber("float");
+                            Console.WriteLine("The answer is: " + Maths.Multiply(Convert.ToSingle(num1), Convert.ToSingle(num2), Convert.ToSingle(num3)));
+                            validEntry = true;
+                            break;
+                        case "e":
+                            exit = true;
+                            validEntry = true;
+                            break;
+                        default:
+                            Console.Write("Please select a valid option: ");
+                            validEntry = false;
+                            break;
+                    }
+                } while (!validEntry);
+            }
+        }
+    }
+}
